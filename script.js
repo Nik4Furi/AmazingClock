f31152bb3f98d43a45deab376bf086dd307c9702
Torrent <torrent23@gmail.com>
2022-07-09 22:07:51 +0530

    Adding the clock.mp3 to listen the sound of the clock

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
+// Set the Alarm sound 
+var audio = document.createElement('audio');
+audio.setAttribute('src', 'clock.mp3');
+
 setInterval(() => {
- let d = new Date();
-  
- htime = d.getHours();
- mtime = d.getMinutes();
- stime = d.getSeconds();
+    audio.play();
+    audio.loop = true;
+    let d = new Date();
 
- hrotation = 30*htime + mtime/2;
- mrotation = 6*mtime;
- srotation = 6*stime;
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
 
- hours.style.transform = `rotate(${hrotation}deg)`;
- minutes.style.transform = `rotate(${mrotation}deg)`;
- seconds.style.transform = `rotate(${srotation}deg)`;
+    hrotation = 30 * htime + mtime / 2;
+    mrotation = 6 * mtime;
+    srotation = 6 * stime;
+
+    hours.style.transform = `rotate(${hrotation}deg)`;
+    minutes.style.transform = `rotate(${mrotation}deg)`;
+    seconds.style.transform = `rotate(${srotation}deg)`;
 
 }, 1000);
 
